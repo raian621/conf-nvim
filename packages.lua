44e60b2238422d92800845a79f0c579558dfd187
Bell, Ryan <ryan.bell62101@gmail.com>
2026-06-14 23:52:45 -0700

    add make autocmd for telescope fzf extension

--- packages.lua
+++ packages.lua
@@ -1,11 +1,12 @@
 -- Callback for handling package changes
-vim.api.nvim_create_autocmd('PackChanged', { callback = function(ev)
-  local name, kind = ev.data.spec.name, ev.data.kind
-
-  if name == 'telescope-fzf-native.nvim' and (kind == 'install' or kind == 'update') then
-    vim.system({ 'make' }, { cwd = ev.data.path })
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(ev)
+    local name, kind = ev.data.spec.name, ev.data.kind
+    if name == 'telescope-fzf-native.nvim' and (kind == 'install' or kind == 'update') then
+      vim.system({ 'make' }, { cwd = ev.data.path })
+    end
   end
-end})
+})
 
 -- Packages
 vim.pack.add({
